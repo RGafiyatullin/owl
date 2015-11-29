daa5428bbfddc70327ef3060824418dc5bc69153
Roman Gafiyatullin <r.gafiyatullin@me.com>
2015-11-29 22:16:23 +0300

    XMPP stream events (error, socket_error) are to contain XmppSrv in themselves

--- include/xmpp_stream_events.hrl
+++ include/xmpp_stream_events.hrl
@@ -4,8 +4,8 @@
 -include_lib ("expellee/include/xml.hrl").
 
 -define( xmpp_closed( Xmpp ), { xmpp_closed, Xmpp } ).
--define( xmpp_socket_error( Xmpp, SocketError ), {xmpp_socket_error, SocketError} ).
--define( xmpp_error( Xmpp, CausingParty, IsTerminal, Type, Details ), {xmpp_error, CausingParty, IsTerminal, Type, Details} ).
+-define( xmpp_socket_error( Xmpp, SocketError ), {xmpp_socket_error, Xmpp, SocketError} ).
+-define( xmpp_error( Xmpp, CausingParty, IsTerminal, Type, Details ), {xmpp_error, Xmpp, CausingParty, IsTerminal, Type, Details} ).
 -define( xmpp_stream_open( Xmpp, Attrs ), {xmpp_stream_open, Xmpp, Attrs} ).
 -define( xmpp_stream_close( Xmpp ), {xmpp_stream_close, Xmpp} ).
 -define( xmpp_stanza( Xmpp, Stanza ), {xmpp_stanza, Xmpp, Stanza} ).
