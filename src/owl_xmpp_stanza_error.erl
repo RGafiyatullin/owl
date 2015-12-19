c57b27337878fe5794412c388e0b1ff48bc5dd77
Roman Gafiyatullin <r.gafiyatullin@me.com>
2015-12-19 11:33:54 +0300

    c2b/1 and b2c/1 added to stream- and stanza-error modules

--- src/owl_xmpp_stanza_error.erl
+++ src/owl_xmpp_stanza_error.erl
@@ -5,19 +5,64 @@
 		property/2,
 		stanza_new/2
 	]).
+-export ([
+		b2c/1,
+		c2b/1
+	]).
 -include("xmpp_stanza_error.hrl").
 -include("ns_xmpp_core.hrl").
 
--define( _ensure( Expr ), case (Expr) of true -> ok; _ -> error( {badarg, ??Expr} ) end ).
--define( _alter_field( FirstField, AsRecord, SecondField, Value ),
-			(Err #xmpp_stanza_error{
-				FirstField = (
-					( Err #xmpp_stanza_error.FirstField )
-						#AsRecord{ SecondField = Value }
-					)
-				}
-			)
-		).
+-spec b2c( binary() ) -> xmpp_stanza_error_condition().
+-spec c2b( xmpp_stanza_error_condition() ) -> binary().
+
+b2c(<<"bad-request">>) -> 'bad-request';
+b2c(<<"conflict">>) -> 'conflict';
+b2c(<<"feature-not-implemented">>) -> 'feature-not-implemented';
+b2c(<<"forbidden">>) -> 'forbidden';
+b2c(<<"gone">>) -> 'gone';
+b2c(<<"internal-server-error">>) -> 'internal-server-error';
+b2c(<<"item-not-found">>) -> 'item-not-found';
+b2c(<<"jid-malformed">>) -> 'jid-malformed';
+b2c(<<"not-acceptable">>) -> 'not-acceptable';
+b2c(<<"not-allowed">>) -> 'not-allowed';
+b2c(<<"not-authorized">>) -> 'not-authorized';
+b2c(<<"policy-violation">>) -> 'policy-violation';
+b2c(<<"recipient-unavailable">>) -> 'recipient-unavailable';
+b2c(<<"redirect">>) -> 'redirect';
+b2c(<<"registration-required">>) -> 'registration-required';
+b2c(<<"remote-server-not-found">>) -> 'remote-server-not-found';
+b2c(<<"remote-server-timeout">>) -> 'remote-server-timeout';
+b2c(<<"resource-constraint">>) -> 'resource-constraint';
+b2c(<<"service-unavailable">>) -> 'service-unavailable';
+b2c(<<"subscription-required">>) -> 'subscription-required';
+b2c(<<"undefined-condition">>) -> 'undefined-condition';
+b2c(<<"unexpected-request">>) -> 'unexpected-request';
+b2c(BadCondition) -> error({badarg, stanza_error_condition, BadCondition}).
+
+
+c2b('bad-request') -> <<"bad-request">>;
+c2b('conflict') -> <<"conflict">>;
+c2b('feature-not-implemented') -> <<"feature-not-implemented">>;
+c2b('forbidden') -> <<"forbidden">>;
+c2b('gone') -> <<"gone">>;
+c2b('internal-server-error') -> <<"internal-server-error">>;
+c2b('item-not-found') -> <<"item-not-found">>;
+c2b('jid-malformed') -> <<"jid-malformed">>;
+c2b('not-acceptable') -> <<"not-acceptable">>;
+c2b('not-allowed') -> <<"not-allowed">>;
+c2b('not-authorized') -> <<"not-authorized">>;
+c2b('policy-violation') -> <<"policy-violation">>;
+c2b('recipient-unavailable') -> <<"recipient-unavailable">>;
+c2b('redirect') -> <<"redirect">>;
+c2b('registration-required') -> <<"registration-required">>;
+c2b('remote-server-not-found') -> <<"remote-server-not-found">>;
+c2b('remote-server-timeout') -> <<"remote-server-timeout">>;
+c2b('resource-constraint') -> <<"resource-constraint">>;
+c2b('service-unavailable') -> <<"service-unavailable">>;
+c2b('subscription-required') -> <<"subscription-required">>;
+c2b('undefined-condition') -> <<"undefined-condition">>;
+c2b('unexpected-request') -> <<"unexpected-request">>.
+
 
 new( Props ) ->
 	lists:foldl(
@@ -54,45 +99,6 @@ property( Property, Error = #xmpp_stanza_error{} ) ->
 			Error #xmpp_stanza_error.common #xmpp_error_common.origin;
 		'common.reason' ->
 			Error #xmpp_stanza_error.common #xmpp_error_common.reason
-	end.
-
-
-new_props_fold( Opt, Err ) ->
-	case Opt of
-		{text, Text} ->
-			?_ensure( is_binary( Text ) orelse Text == undefined ),
-			?_alter_field( public, xmpp_stanza_error_pub, text, Text );
-		{condition, Condition} ->
-			?_ensure( ?is_xmpp_stanza_error_condition( Condition ) ),
-			?_alter_field( public, xmpp_stanza_error_pub, condition, Condition );
-		{code, Code} ->
-			?_ensure( is_integer( Code ) orelse Code == undefined ),
-			?_alter_field( public, xmpp_stanza_error_pub, code, Code );
-		{type, Type} ->
-			?_ensure( ?is_xmpp_stanza_error_type( Type ) ),
-			?_alter_field( public, xmpp_stanza_error_pub, type, Type );
-		{xml_children, Xmls} ->
-			?_ensure( is_list( Xmls ) ),
-			?_alter_field( public, xmpp_stanza_error_pub, xml_children, Xmls );
-		{condition_xml_children, Xmls} ->
-			?_ensure( is_list( Xmls ) ),
-			?_alter_field( public, xmpp_stanza_error_pub, condition_xml_children, Xmls );
-		{stanza_xml_children, Xmls} ->
-			?_ensure( is_list( Xmls ) ),
-			?_alter_field( public, xmpp_stanza_error_pub, stanza_xml_children, Xmls );
-
-		{'common.stack', Stack} ->
-			?_ensure( is_list(Stack) orelse Stack == undefined ),
-			?_alter_field( common, xmpp_error_common, stack, Stack );
-		{'common.origin', Origin} ->
-			?_alter_field( common, xmpp_error_common, stack, Origin );
-		{'common.reason', Reason} ->
-			?_alter_field( common, xmpp_error_common, reason, Reason );
-
-		Unknown ->
-			Props0 = ( Err #xmpp_stanza_error.common ) #xmpp_error_common.unknown_props,
-			Props1 = [ Unknown | Props0 ],
-			?_alter_field( common, xmpp_error_common, unknown_props, Props1 )
 	end.
 
 stanza_new( Err, RequestStanza ) ->
@@ -139,5 +145,57 @@ maybe_attr( NewAttrName, OldAttrName, RequestStanza ) ->
 	case exp_node_attrs:attr( OldAttrName, RequestStanza ) of
 		undefined -> [];
 		Defined -> [ {NewAttrName, Defined} ]
+	end.
+
+
+
+
+-define( _ensure( Expr ), case (Expr) of true -> ok; _ -> error( {badarg, ??Expr} ) end ).
+-define( _alter_field( FirstField, AsRecord, SecondField, Value ),
+			(Err #xmpp_stanza_error{
+				FirstField = (
+					( Err #xmpp_stanza_error.FirstField )
+						#AsRecord{ SecondField = Value }
+					)
+				}
+			)
+		).
+
+new_props_fold( Opt, Err ) ->
+	case Opt of
+		{text, Text} ->
+			?_ensure( is_binary( Text ) orelse Text == undefined ),
+			?_alter_field( public, xmpp_stanza_error_pub, text, Text );
+		{condition, Condition} ->
+			?_ensure( ?is_xmpp_stanza_error_condition( Condition ) ),
+			?_alter_field( public, xmpp_stanza_error_pub, condition, Condition );
+		{code, Code} ->
+			?_ensure( is_integer( Code ) orelse Code == undefined ),
+			?_alter_field( public, xmpp_stanza_error_pub, code, Code );
+		{type, Type} ->
+			?_ensure( ?is_xmpp_stanza_error_type( Type ) ),
+			?_alter_field( public, xmpp_stanza_error_pub, type, Type );
+		{xml_children, Xmls} ->
+			?_ensure( is_list( Xmls ) ),
+			?_alter_field( public, xmpp_stanza_error_pub, xml_children, Xmls );
+		{condition_xml_children, Xmls} ->
+			?_ensure( is_list( Xmls ) ),
+			?_alter_field( public, xmpp_stanza_error_pub, condition_xml_children, Xmls );
+		{stanza_xml_children, Xmls} ->
+			?_ensure( is_list( Xmls ) ),
+			?_alter_field( public, xmpp_stanza_error_pub, stanza_xml_children, Xmls );
+
+		{'common.stack', Stack} ->
+			?_ensure( is_list(Stack) orelse Stack == undefined ),
+			?_alter_field( common, xmpp_error_common, stack, Stack );
+		{'common.origin', Origin} ->
+			?_alter_field( common, xmpp_error_common, stack, Origin );
+		{'common.reason', Reason} ->
+			?_alter_field( common, xmpp_error_common, reason, Reason );
+
+		Unknown ->
+			Props0 = ( Err #xmpp_stanza_error.common ) #xmpp_error_common.unknown_props,
+			Props1 = [ Unknown | Props0 ],
+			?_alter_field( common, xmpp_error_common, unknown_props, Props1 )
 	end.
 
--- src/owl_xmpp_stream_error.erl
+++ src/owl_xmpp_stream_error.erl
@@ -1,8 +1,5 @@
 -module (owl_xmpp_stream_error).
 -compile ({parse_transform, gin}).
--include ("xmpp_stream_error.hrl").
--include ("ns_xmpp_core.hrl").
-
 -export ([
 		new/1,
 		raise/1,
@@ -10,17 +7,67 @@
 
 		stanza_new/1
 	]).
+-export ([
+		b2c/1,
+		c2b/1
+	]).
+-include ("xmpp_stream_error.hrl").
+-include ("ns_xmpp_core.hrl").
 
--define( _ensure( Expr ), case (Expr) of true -> ok; _ -> error( {badarg, ??Expr} ) end ).
--define( _alter_field( FirstField, AsRecord, SecondField, Value ),
-			(Err #xmpp_stream_error{
-				FirstField = (
-					( Err #xmpp_stream_error.FirstField )
-						#AsRecord{ SecondField = Value }
-					)
-				}
-			)
-		).
+-spec b2c( binary() ) -> xmpp_stream_error_condition().
+-spec c2b( xmpp_stream_error_condition() ) -> binary().
+
+b2c(<<"bad-format">>) -> 'bad-format';
+b2c(<<"bad-namespace-prefix">>) -> 'bad-namespace-prefix';
+b2c(<<"conflict">>) -> 'conflict';
+b2c(<<"connection-timeout">>) -> 'connection-timeout';
+b2c(<<"host-gone">>) -> 'host-gone';
+b2c(<<"host-unknown">>) -> 'host-unknown';
+b2c(<<"improper-addressing">>) -> 'improper-addressing';
+b2c(<<"internal-server-error">>) -> 'internal-server-error';
+b2c(<<"invalid-form">>) -> 'invalid-form';
+b2c(<<"invalid-id">>) -> 'invalid-id';
+b2c(<<"invalid-namespace">>) -> 'invalid-namespace';
+b2c(<<"invalid-xml">>) -> 'invalid-xml';
+b2c(<<"not-authorized">>) -> 'not-authorized';
+b2c(<<"policy-violation">>) -> 'policy-violation';
+b2c(<<"remote-connection-failed">>) -> 'remote-connection-failed';
+b2c(<<"resource-constraint">>) -> 'resource-constraint';
+b2c(<<"restricted-xml">>) -> 'restricted-xml';
+b2c(<<"see-other-host">>) -> 'see-other-host';
+b2c(<<"system-shutdown">>) -> 'system-shutdown';
+b2c(<<"undefined-condition">>) -> 'undefined-condition';
+b2c(<<"unsupported-encoding">>) -> 'unsupported-encoding';
+b2c(<<"unsupported-stanza-type">>) -> 'unsupported-stanza-type';
+b2c(<<"unsupported-version">>) -> 'unsupported-version';
+b2c(<<"not-well-formed">>) -> 'not-well-formed';
+b2c(BadCondition) -> error({badarg, stream_error_condition, BadCondition}).
+
+
+c2b('bad-format') -> <<"bad-format">>;
+c2b('bad-namespace-prefix') -> <<"bad-namespace-prefix">>;
+c2b('conflict') -> <<"conflict">>;
+c2b('connection-timeout') -> <<"connection-timeout">>;
+c2b('host-gone') -> <<"host-gone">>;
+c2b('host-unknown') -> <<"host-unknown">>;
+c2b('improper-addressing') -> <<"improper-addressing">>;
+c2b('internal-server-error') -> <<"internal-server-error">>;
+c2b('invalid-form') -> <<"invalid-form">>;
+c2b('invalid-id') -> <<"invalid-id">>;
+c2b('invalid-namespace') -> <<"invalid-namespace">>;
+c2b('invalid-xml') -> <<"invalid-xml">>;
+c2b('not-authorized') -> <<"not-authorized">>;
+c2b('policy-violation') -> <<"policy-violation">>;
+c2b('remote-connection-failed') -> <<"remote-connection-failed">>;
+c2b('resource-constraint') -> <<"resource-constraint">>;
+c2b('restricted-xml') -> <<"restricted-xml">>;
+c2b('see-other-host') -> <<"see-other-host">>;
+c2b('system-shutdown') -> <<"system-shutdown">>;
+c2b('undefined-condition') -> <<"undefined-condition">>;
+c2b('unsupported-encoding') -> <<"unsupported-encoding">>;
+c2b('unsupported-stanza-type') -> <<"unsupported-stanza-type">>;
+c2b('unsupported-version') -> <<"unsupported-version">>;
+c2b('not-well-formed') -> <<"not-well-formed">>.
 
 new( Props ) ->
 	lists:foldl(
@@ -52,6 +99,41 @@ property( Property, Error = #xmpp_stream_error{} ) ->
 			Error #xmpp_stream_error.common #xmpp_error_common.reason
 	end.
 
+stanza_new( Err ) ->
+	Condition = property( condition, Err ),
+	ConditionChildren = property( condition_xml_children, Err ),
+	ConditionChild = exp_node:new(
+		{?ns_xmpp_streams, atom_to_binary( Condition, latin1 )},
+		[], ConditionChildren ),
+
+	Text = property( text, Err ),
+	XmlChildren0 = property( xml_children, Err ),
+	XmlChildren1 =
+		case property( text, Err ) of
+			undefined -> XmlChildren0;
+			Text ->
+				TextChild = exp_node:new(
+						{?ns_xmpp_streams, <<"text">>}, [],
+						[ exp_text_node:cdata_new( Text ) ]
+					),
+				[ TextChild | XmlChildren0 ]
+		end,
+	exp_node:new( {?ns_jabber_streams, <<"error">>}, [], [
+			ConditionChild
+			| XmlChildren1
+		] ).
+
+-define( _ensure( Expr ), case (Expr) of true -> ok; _ -> error( {badarg, ??Expr} ) end ).
+-define( _alter_field( FirstField, AsRecord, SecondField, Value ),
+			(Err #xmpp_stream_error{
+				FirstField = (
+					( Err #xmpp_stream_error.FirstField )
+						#AsRecord{ SecondField = Value }
+					)
+				}
+			)
+		).
+
 new_props_fold( Opt, Err ) ->
 	case Opt of
  		{text, Text} ->
@@ -79,27 +161,3 @@ new_props_fold( Opt, Err ) ->
 			Props1 = [ Unknown | Props0 ],
 			?_alter_field( common, xmpp_error_common, unknown_props, Props1 )
 	end.
-
-stanza_new( Err ) ->
-	Condition = property( condition, Err ),
-	ConditionChildren = property( condition_xml_children, Err ),
-	ConditionChild = exp_node:new(
-		{?ns_xmpp_streams, atom_to_binary( Condition, latin1 )},
-		[], ConditionChildren ),
-
-	Text = property( text, Err ),
-	XmlChildren0 = property( xml_children, Err ),
-	XmlChildren1 =
-		case property( text, Err ) of
-			undefined -> XmlChildren0;
-			Text ->
-				TextChild = exp_node:new(
-						{?ns_xmpp_streams, <<"text">>}, [],
-						[ exp_text_node:cdata_new( Text ) ]
-					),
-				[ TextChild | XmlChildren0 ]
-		end,
-	exp_node:new( {?ns_jabber_streams, <<"error">>}, [], [
-			ConditionChild
-			| XmlChildren1
-		] ).
